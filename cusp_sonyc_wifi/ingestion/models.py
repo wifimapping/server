90a376420d62bc57d88c88ca3cf9f291820ec0d8
Jeremy Neiman <docmarionum1@gmail.com>
2016-06-02 14:33:28 -0400

    create table

--- cusp_sonyc_wifi/ingestion/models.py
+++ cusp_sonyc_wifi/ingestion/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class UniqueLocations(models.Model):
+    idx = models.AutoField(primary_key=True)
+    lat = models.DecimalField(max_digits=8, decimal_places=4)
+    lng = models.DecimalField(max_digits=8, decimal_places=4)
+
+    class Meta:
+        unique_together = ('lat', 'lng')
+        managed = True
+        db_table = 'unique_locations'
+
+    def __unicode__(self):
+        return " / " + str(self.lat) + " / " + str(self.lng)
+
 class WifiScan(models.Model):
     idx = models.BigIntegerField(primary_key=True)
     lat = models.FloatField()
@@ -23,15 +36,3 @@ class WifiScan(models.Model):
 
     def __unicode__(self):
         return str(self.idx) + " / " + str(self.lat) + " / " + str(self.lng) + " / " + str(self.acc) + " / " + str(self.altitude) + " / " + str(self.time) + " / " + self.device_mac + " / " + self.app_version+ " / " + self.droid_version+ " / " + self.device_model+ " / " + self.ssid+ " / " + self.bssid+ " / " + self.caps+ " / " + str(self.level)+ " / " + str(self.freq)
-
-
-class UniqueLocations(models.Model):
-    lat = models.FloatField(primary_key=True)
-    lng = models.FloatField(primary_key=True)
-
-    class Meta:
-        managed = True
-        db_table = 'unique_locations'
-
-    def __unicode__(self):
-        return " / " + str(self.lat) + " / " + str(self.lng)
